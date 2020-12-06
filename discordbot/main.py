ddf254def38f5f68b3c423faf0dba3647d474d07
butsuli-shine <cheichey.1223@gmail.com>
2020-12-06 23:27:42 +0900

    /nipple,/help,/youtube,/youtube --omanko,/sourceコマンドを追加.

--- discordbot/main.py
+++ discordbot/main.py
@@ -12,12 +12,32 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    send_message: str = ""
     if message.author.bot:
         return
 
-    if message.content == '/OPPAI':
-        await message.channel.send("othinpo")
+    if message.content == '/nipple':
+        send_message: str = "nipple"
 
+    if message.content == '/help':
+        send_message += "NarurungoBotです.あなたの願いをかなえます.\n"
+        send_message += "`/nipple`: nippleコマンドです.\n"
+        send_message += "" \
+                        "`/youtube`: youtubeのURLを出します.\n"\
+                        "option: `--omanko`\n"\
+                        "`/source` : ソースを出します.\n"\
+                        ""
+
+    if message.content == '/youtube':
+        send_message: str = "https://youtube.com"
+
+    if message.content == '/youtube --omanko':
+        send_message = "http://pornhub.com"
+
+    if message.content == '/source':
+        send_message = "https://github.com/butsuli-shine/discordbot"
+
+    await message.channel.send(send_message)
 
 if __name__ == "__main__":
     client.run(token)
